7ee111b3af7bbe77aa1e12dedb6a3c7e8274fede
MarcioLeonidio <138794071+MarcioLeonidio@users.noreply.github.com>
2025-05-13 21:30:54 +0000

    modified:   chinook.db modified:   exercicios/1-CRUD/3-inserir-dados.sql

--- exercicios/1-CRUD/3-inserir-dados.sql
+++ exercicios/1-CRUD/3-inserir-dados.sql
@@ -1,4 +1,9 @@
 -- Insira 3 classes salariais na tabela salario_classe
-
-
+INSERT INTO salario_classe (nivel,salario) 
+VALUES 
+('Estag', 4500),
+('Pleno', 6000),
+('Senior', 8000),
+('Gerente', 12000),
+('CEO', 25000);
 -- Insira 1 nova classe salarial, sem declarar o atributo id_salario
